441f57fc9584222abd44655d04d0be281682dbab
Alexandru Craevschi <craevschi.alexandru@gmail.com>
2025-11-24 15:51:49 +0100

    run with interaction

--- analysis/run_brms.R
+++ analysis/run_brms.R
@@ -4,6 +4,7 @@ library(brms)
 
 model_data <- read.csv("analysis/pauls_principle_dataset.csv")
 model_data <- unique(model_data)
+model_data$is_bipartite <- as.factor(model_data$is_bipartite)
 
 ####### MODEL ########
 
@@ -15,7 +16,13 @@ hierarchical_model_formula <- bf(
   family = bernoulli()
 )
 
-# 2. Run the hierarchical model
+hierarchical_model_interaction_formula <- bf(
+  has_levelled ~ is_bipartite + s(date, by = is_bipartite) + 
+                 dialect + log_freq + (1 | lemma_std),
+  family = bernoulli()
+)
+
+# 2. Run the hierarchical and interaction model
 # ------------------------------------
 hierarchical_model_fit <- brm(
   formula = hierarchical_model_formula,
@@ -28,10 +35,26 @@ hierarchical_model_fit <- brm(
   file = "analysis/models/hierarchical_brms_fit"
 )
 
+hierarchical_model_interaction_fit <- brm(
+  formula = hierarchical_model_interaction_formula,
+  data = model_data,
+  chains = 4,
+  iter = 2000,
+  cores = 4,
+  backend = "rstan",
+  control = list(adapt_delta = 0.99),
+  file = "analysis/models/hierarchical_interaction_brms_fit"
+)
+
+
 # 3. View the results
 # ------------------------------------
 print(summary(hierarchical_model_fit))
 
+print(summary(hierarchical_model_interaction_fit))
+
 # 4. Plot the fixed effects (like 'is_bipartite') and the spline
 # ------------------------------------
 plot(conditional_effects(hierarchical_model_fit), points = TRUE)
+
+plot(conditional_effects(hierarchical_model_interaction_fit), points = TRUE)
