869059023c292f8abc79c655ac05110b7193ab35
Alexandru Craevschi <craevschi.alexandru@gmail.com>
2026-03-24 12:10:25 +0100

    clean up repo

--- run_tasks.sh
+++
@@ -1,6 +0,0 @@
-sbatch task.sh analysis/obs_aggr.csv analysis/seq_aggr.csv analysis/models/std_form_freq_kappa_high.stan
-sbatch task.sh analysis/obs_aggr.csv analysis/seq_aggr.csv analysis/models/std_freq_kappa_high.stan
-sbatch task.sh analysis/obs_aggr.csv analysis/seq_aggr.csv analysis/models/std_lemma_freq_kappa_high.stan
-sbatch task.sh analysis/obs_aggr_var.csv analysis/seq_aggr_var.csv analysis/models/std_form_freq_kappa_high.stan
-sbatch task.sh analysis/obs_aggr_var.csv analysis/seq_aggr_var.csv analysis/models/std_freq_kappa_high.stan
-sbatch task.sh analysis/obs_aggr_var.csv analysis/seq_aggr_var.csv analysis/models/std_lemma_freq_kappa_high.stan
